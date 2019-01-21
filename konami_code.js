63929632867a049ea8b8797ce5521ff60b58e178
Carol Greene <carolgreene3@gmail.com>
2019-01-21 22:16:00 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -7,7 +7,6 @@ function init() {
 document.body.addEventListener('keydown', function(e) {
   const key = parseInt(e.detail || e.which);
 
-
   if (key === code[index])  {  //ck to see if key entered matches code at that index
     //debugger           **use debugger & hover over variables for values
     index++                    //increment index for next entry
@@ -16,7 +15,7 @@ document.body.addEventListener('keydown', function(e) {
     if (index === code.length) {      //ck to see if all entries have been made
         alert("Hurray!")                //alert message
         index = 0                     //reset index to 0 for next try
-
+      }
     } else {
     //alert('wrong entry, try again!')
       index = 0//reset index to 0 for next try
